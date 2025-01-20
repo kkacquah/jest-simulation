c3cf7cf9de9e6435c9cfb64fefb3967069f6cad9
Kenneth Acquah <ken@trucksmarter.com>
2025-01-20 08:48:04 -0800

    Remove node_modules and dist directories from repository

--- dist/__tests___/e2e/utils.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function normalizeIcons(str: string): string;
--- dist/assertions/expectEventually.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const expectEventually: <T>(fn: () => Promise<T> | T, timeout?: number, interval?: number) => Promise<void>;
